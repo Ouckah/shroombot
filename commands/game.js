e7c1d84d3eba6967c4d04f00d375ca933deb613f
MushroomPlays <43829909+MushroomPlays@users.noreply.github.com>
2020-04-11 03:52:29 -0400

    Delete game.js

--- commands/game.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message, args, user, channel, self) => {
-    client.game().then(function(channel) {
-        let game = channel.game
-        client.say(channel, `@${user.username} - Mushroom is playing ${game} right now. He's a bot.`)
-    })
-}
